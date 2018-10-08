91db84d9697fd4193d2a0f4d65316e721549500d
Gonzalo <gonzaperezgmail.com>
2018-10-07 23:05:24 -0300

    fix truffle

--- truffle.js
+++ truffle.js
@@ -15,16 +15,24 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
     networks: {
         development: {
             host: "127.0.0.1",
             port: 8545,
-            network_id: "*" // Match any network id
+            network_id: "*", // Match any network id
+			gas: 4698712
         },
         ganache: {
             host: "127.0.0.1",
             port: 7545,
-            network_id: "*" // Match any network id
+            network_id: "*", // Match any network id
+			gas: 4698712
         },
     }
 };
